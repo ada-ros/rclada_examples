eb401a753e9514c93f6f69db6bd2055c00669f22
Alejandro R Mosteo <alejandro@mosteo.com>
2018-05-28 18:17:23 +0200

    Added files

---
+++ src/c_helper.c
@@ -0,0 +1,17 @@
+// Typesupport experiments
+#include <rosidl_generator_c/message_type_support_struct.h>
+#include <std_msgs/msg/string.h>
+
+// Introspection experiments
+#include <rosidl_typesupport_introspection_c/message_introspection.h>
+#include <std_msgs/msg/string__rosidl_typesupport_introspection_c.h>
+
+#include <stdio.h>
+
+void c_helper(void) {
+//  const rosidl_message_type_support_t * string_ts = ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String);  
+//  printf("Typesupport in C: %p\n", (void*)string_ts); 
+  
+//  const rosidl_message_type_support_t * string_tsi = 
+//  	ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, std_msgs, msg, String);
+}
---
+++ src/listener_dyn.adb
@@ -0,0 +1,46 @@
+with RCL.Logging;
+with RCL.Nodes;
+with RCL.Subscriptions;
+
+with ROSIDL.Dynamic;
+
+-- This example uses low level subscription and dynamic messages
+
+procedure Listener_Dyn is
+   use RCL;
+
+   Msg : ROSIDL.Dynamic.Message :=
+           ROSIDL.Dynamic.Init ("std_msgs", "String");
+begin
+   Logging.Set_Name ("listener_raw");
+   Logging.Info ("Node starting...");
+
+   declare
+      Node : Nodes.Node := Nodes.Init ("listener");
+   begin
+      Logging.Info ("Node started");
+
+--      Logging.Info ("SUPPORT: " & System.Address_Image (Rosidl.Std_Msgs.Msg.Typesupport_String.all'Address));
+      declare
+         Sub  : Subscriptions.Subscription :=
+                  Subscriptions.Init (Node,
+                                      Msg.Typesupport,
+                                      "chatter");
+
+         Info : Subscriptions.Message_Info;
+      begin
+         Logging.Info ("Subscription started");
+
+         while True loop
+            delay 1.0;
+            if Sub.Take_Raw (Msg.To_Ptr, Info) then
+               Logging.Info ("Got chatter! [" & Msg ("data").Get_String & "]");
+            else
+               Logging.Info (":'(");
+            end if;
+         end loop;
+      end;
+   end;
+
+   Logging.Info ("Node shut down");
+end Listener_Dyn;
