d54129f04d147fcb829d92478139fb71e5d88ce0
Alejandro R. Mosteo <amosteo@unizar.es>
2018-07-13 12:30:56 +0200

    Don't use containers for graph node name list, add iterator

--- src/graph_info.adb
+++ src/graph_info.adb
@@ -5,7 +5,7 @@ with RCL.Utils;
 procedure Graph_Info is
    use RCL;
 
-   Node : Nodes.Node := Nodes.Init (Utils.Command_Name);
+   Node : constant Nodes.Node := Nodes.Init (Utils.Command_Name);
 begin
    Logging.Info ("Starting...");
    delay 2.0; -- Let it soak some network info
@@ -16,25 +16,25 @@ begin
    
    Logging.Info ("---");
    
-   declare
-      Services : constant Utils.Services_And_Types := Node.Graph_Services;
-   begin 
-      for I in Services.Iterate loop 
-         Logging.Info ("Service: " & Utils.String_Maps.Key (I) & 
-                       "; type: "      & Services (I));
-      end loop;
-   end;
-   
-   Logging.Info ("---");
-   
-   declare
-      Topics : constant Utils.Topics_And_Types := Node.Graph_Topics;
-   begin 
-      for I in Topics.Iterate loop 
-         Logging.Info ("Topic: " & Utils.String_Maps.Key (I) & 
-                         "; type: "      & Topics (I) &
-                         "; publishers:" & Node.Graph_Count_Publishers (Utils.String_Maps.Key (I))'Img & 
-                         "; subscribers:" & Node.Graph_Count_Subscribers (Utils.String_Maps.Key (I))'Img);
-      end loop;
-   end;
+--     declare
+--        Services : constant Utils.Services_And_Types := Node.Graph_Services;
+--     begin 
+--        for I in Services.Iterate loop 
+--           Logging.Info ("Service: " & Utils.String_Maps.Key (I) & 
+--                         "; type: "      & Services (I));
+--        end loop;
+--     end;
+--     
+--     Logging.Info ("---");
+--     
+--     declare
+--        Topics : constant Utils.Topics_And_Types := Node.Graph_Topics;
+--     begin 
+--        for I in Topics.Iterate loop 
+--           Logging.Info ("Topic: " & Utils.String_Maps.Key (I) & 
+--                           "; type: "      & Topics (I) &
+--                           "; publishers:" & Node.Graph_Count_Publishers (Utils.String_Maps.Key (I))'Img & 
+--                           "; subscribers:" & Node.Graph_Count_Subscribers (Utils.String_Maps.Key (I))'Img);
+--        end loop;
+--     end;
 end Graph_Info;
